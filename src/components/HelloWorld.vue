20c3adc64d41f1d8e6882cd46e38df11bc9ac1d5
李卓 <1837491827@qqcom>
2019-11-15 00:23:40 +0800

    npm 测试代码半成品(数组去重)提交

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,42 +1,45 @@
 <template>
-  <div class="hello">
-    <h1>{{ msg }}</h1>
-    <h2>{{$store.state.app.name}}</h2>
+  <div>
+    <input class="form-control" id="inputEmail3" placeholder="请输入账号" v-model="account">
+    <input type="password" class="form-control" id="inputPassword3" placeholder="请输入密码" v-model="password">
+    <button type="submit" class="btn btn-default" @click="login">登录</button>
   </div>
 </template>
+
 <script>
+import jsonArray from '../utils/jsonArray'
 export default {
-  name: 'HelloWorld',
   data () {
     return {
-      msg: '李卓vue项目架构'
+      account: '',
+      password: '',
+      arr: [{ 'id': 1, 'name': '李卓' }, { 'id': 5, 'name': 's' }, { 'id': 3, 'name': '李金伟' }]
     }
   },
   mounted () {
-    console.log(this.getUser())
+    console.log(jsonArray.excludeSame([]))
   },
   methods: {
-    getUser () {
-      return this.$store.state.app.name
+    login () {
+      // 获取已有账号密码
+      this.$http.get('/api/login/getAccount')
+        .then((response) => {
+          // 响应成功回调
+          console.log(response)
+          let params = {
+            account: this.account,
+            password: this.password
+          }
+          // 创建一个账号密码
+          return this.$http.post('/api/login/createAccount', params)
+        })
+        .then((response) => {
+          console.log(response)
+        })
+        .catch((reject) => {
+          console.log(reject)
+        })
     }
   }
 }
 </script>
-
-<!-- Add "scoped" attribute to limit CSS to this component only -->
-<style scoped>
-h1, h2 {
-  font-weight: normal;
-}
-ul {
-  list-style-type: none;
-  padding: 0;
-}
-li {
-  display: inline-block;
-  margin: 0 10px;
-}
-a {
-  color: #42b983;
-}
-</style>
